636569d2c822e0c77da0a8656b070cf77ceb0b9b
Deron <deron8523114888@gmail.com>
2021-11-16 00:14:23 +0800

    [feat] commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hello world!');
+let strA = 'Hello, '
+strA += 'GQSM!'
+
+console.log(strA)
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,37 @@
 const path = require('path');
 module.exports = {
     //這個 webpack 要打包的對象
-    entry: {
-        index: './index.js'
-    },
+    entry: ['./index.js', './app.jsx'],
     output: {
         // 打包後的檔案名稱
         filename: 'bundle.js',
         // 打包後的路徑，這裡使用 path 模組的 resolve() 取得絕對位置，也就是目前專案的根目錄
-        path: path.resolve('./'),
+        path: path.resolve(__dirname, './'),
+    },
+    module: {
+        rules: [
+            //第一個loader編譯JSX
+            { 
+                test: /.jsx$/, 
+                exclude: /node_modules/, 
+                use: { 
+                    loader: 'babel-loader', 
+                    options: { 
+                        presets: ['@babel/preset-react', "@babel/preset-env"] 
+                    } 
+                } 
+            },
+            //第二個loader編譯ES6
+            { 
+                test: /.js$/, 
+                exclude: /node_modules/, 
+                use: { 
+                    loader: 'babel-loader', 
+                    options: { 
+                        presets: ['@babel/preset-env'] 
+                    } 
+                } 
+            }
+        ]
     }
 };
